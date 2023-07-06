fd2c891d1149a3db4dd32d98deb8918c89547678
Dat240906 <phungthanhdat001@gmail.com>
2023-07-06 18:48:19 +0700

    fix pos

--- form_auth/form/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('login/', Auth.as_view(), name= 'login'),
-    path('product/', Product.as_view(), name='product')
-]
